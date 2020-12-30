486cf665fab0df0c06a84220b832094d3f2dec11
AbdurashidovShahzod <shahzod9933@gmail.com>
2020-12-31 02:34:04 +0500

    data

---
+++ app/src/main/java/uz/unzosoft/zolunch/uiphone/ui/main/mainphoneui/MainPhoneActivityUi.kt
@@ -0,0 +1,12 @@
+package uz.unzosoft.zolunch.uiphone.ui.main.mainphoneui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.unzosoft.zolunch.R
+
+class MainPhoneActivityUi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_phone_ui)
+    }
+}
